1a6bcfcd91551e2cf4011d6b59eeac1dd481118f
garyyb <garyybai@gmail.com>
2017-08-06 11:07:57 +1000

    do functions

--- main.jl
+++ main.jl
@@ -1,16 +1,16 @@
 include("Network.jl")
 import FeedForward
 
-function sigmoid()
-
+function sigmoid(x::Vector{Float64})
+    return 1.0./(1.0 .+ exp(-1 .* x))
 end
 
-function sigmoid_prime()
-
+function sigmoid_prime(x::Vector{Float64})
+    return sigmoid(x).*(1 .- sigmoid(z))
 end
 
-function cost_derivative()
-
+function cost_derivative(x::Vector{Float64}, y::Vector{Float64})
+    return x - y
 end
 
 function main()
